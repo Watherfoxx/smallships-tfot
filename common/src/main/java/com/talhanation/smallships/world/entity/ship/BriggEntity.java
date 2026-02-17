a707402048fe660ce49f7a2adb5aa562b386af7d
Watherfoxx <46624490+Watherfoxx@users.noreply.github.com>
2026-02-17 16:37:37 +0100

    Adjust cannon limits for cog/brigg and rename drakkar label in French

--- common/src/main/java/com/talhanation/smallships/world/entity/ship/BriggEntity.java
+++ common/src/main/java/com/talhanation/smallships/world/entity/ship/BriggEntity.java
@@ -207,27 +207,22 @@ public class BriggEntity extends ContainerShip implements Bannerable, Sailable, 
      **/
     public CannonPosition getCannonPosition(int index){
         List<CannonPosition> positionList = new ArrayList<>();
-        CannonPosition pos1 = new CannonPosition(1.4, 0, 0.75, true);
-        CannonPosition pos2 = new CannonPosition(1.4, 0, 0.75, false);
+        CannonPosition pos1 = new CannonPosition(-0.1, 0, 0.85, true);
+        CannonPosition pos2 = new CannonPosition(-0.1, 0, 0.85, false);
 
-        CannonPosition pos3 = new CannonPosition(-0.1, 0, 0.85, true);
-        CannonPosition pos4 = new CannonPosition(-0.1, 0, 0.85, false);
-
-        CannonPosition pos5 = new CannonPosition(-1.2, 0, 0.75, true);
-        CannonPosition pos6 = new CannonPosition(-1.2, 0, 0.75, false);
+        CannonPosition pos3 = new CannonPosition(-1.2, 0, 0.75, true);
+        CannonPosition pos4 = new CannonPosition(-1.2, 0, 0.75, false);
         positionList.add(pos1);
         positionList.add(pos2);
         positionList.add(pos3);
         positionList.add(pos4);
-        positionList.add(pos5);
-        positionList.add(pos6);
 
         return positionList.get(index);
     }
 
     @Override
     public byte getMaxCannonPerSide(){
-        return 3;
+        return 2;
     }
 
     public @Nullable Vec3 applyLeashOffset() {
