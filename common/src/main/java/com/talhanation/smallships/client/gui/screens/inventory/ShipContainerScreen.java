efdfae430b1d44e2569ab89a1ad0a58a0dce4110
Watherfoxx <46624490+Watherfoxx@users.noreply.github.com>
2026-02-17 16:27:56 +0100

    Ajuste l'UI d'inventaire des bateaux

--- common/src/main/java/com/talhanation/smallships/client/gui/screens/inventory/ShipContainerScreen.java
+++ common/src/main/java/com/talhanation/smallships/client/gui/screens/inventory/ShipContainerScreen.java
@@ -4,8 +4,6 @@ import com.talhanation.smallships.SmallShipsMod;
 import com.talhanation.smallships.config.SmallShipsConfig;
 import com.talhanation.smallships.math.Kalkuel;
 import com.talhanation.smallships.world.entity.ship.ContainerShip;
-import com.talhanation.smallships.world.entity.ship.abilities.Cannonable;
-import com.talhanation.smallships.world.entity.ship.abilities.Shieldable;
 import com.talhanation.smallships.world.inventory.ShipContainerMenu;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
@@ -61,41 +59,27 @@ public class ShipContainerScreen extends AbstractContainerScreen<ShipContainerMe
             return;
         }
 
-        Button backward = this.addRenderableWidget(new Button.Builder(Component.literal("<"),
-                button -> this.getMenu().clickMenuButton(this.minecraft.player, -1))
-                .pos(leftPos + 115, topPos + 125).size(12, 12)
-                .build());
+        if (this.pageCount > 1) {
+            Button backward = this.addRenderableWidget(new Button.Builder(Component.literal("<"),
+                    button -> this.getMenu().clickMenuButton(this.minecraft.player, -1))
+                    .pos(leftPos + 115, topPos + 125).size(12, 12)
+                    .build());
 
-        backward.active = this.pageCount > 1 && this.pageIndex + 1 > 1;
+            backward.active = this.pageIndex + 1 > 1;
 
-        Button forward = this.addRenderableWidget(new Button.Builder(Component.literal(">"),
-                button -> this.getMenu().clickMenuButton(this.minecraft.player, 1))
-                .pos(leftPos + 157, topPos + 125)
-                .size(12, 12)
-                .build());
-        forward.active = this.pageCount > 1 && this.pageIndex + 1 < this.pageCount;
+            Button forward = this.addRenderableWidget(new Button.Builder(Component.literal(">"),
+                    button -> this.getMenu().clickMenuButton(this.minecraft.player, 1))
+                    .pos(leftPos + 157, topPos + 125)
+                    .size(12, 12)
+                    .build());
+            forward.active = this.pageIndex + 1 < this.pageCount;
+        }
     }
     @Override
     protected void renderLabels(GuiGraphics guiGraphics, int i, int j) {
         super.renderLabels(guiGraphics, i, j);
-        String name = this.containerShip.getDisplayName().getString();
-
-        String smallShipTypeRaw = this.containerShip.getType().getDescription().getString();
-        String smallShipType = smallShipTypeRaw.substring(0,1).toUpperCase() + smallShipTypeRaw.substring(1);
-
         int currentPassengers = this.containerShip.getPassengers().size();
         int maxPassengers = this.containerShip.getMaxPassengers();
-
-        int maxAttachment = 0;
-        int currentAttachment = 0;
-        if(this.containerShip instanceof Cannonable cannonable){
-            maxAttachment =  cannonable.getMaxCannonPerSide() * 2;
-            currentAttachment = cannonable.getCannonCount();
-        }
-        else if (this.containerShip instanceof Shieldable shieldable){
-            maxAttachment =  shieldable.getMaxShieldsPerSide() * 2;
-            currentAttachment = shieldable.getShields().size();
-        }
 
         int dmg = (int) (this.containerShip.getDamage() * 100 / this.containerShip.getAttributes().maxHealth);
 
@@ -131,21 +115,13 @@ public class ShipContainerScreen extends AbstractContainerScreen<ShipContainerMe
         int gap = 14;
         guiGraphics.pose().pushPose();
         guiGraphics.pose().scale(0.7F, 0.7F, 1F);
-        String attachment = this.containerShip instanceof Shieldable ? "Shields:" : "Cannons:";
+        guiGraphics.drawString(font, "Crew:", leftPos, topPos + gap * 0, FONT_COLOR, false);
+        guiGraphics.drawString(font, "Speed " + unit + ":", leftPos, topPos + gap * 1, FONT_COLOR, false);
+        guiGraphics.drawString(font, "Damage:", leftPos, topPos + gap * 2, FONT_COLOR, false);
 
-        guiGraphics.drawString(font, "Name:", leftPos, topPos + gap * 0, FONT_COLOR, false);
-        guiGraphics.drawString(font, "Type:", leftPos, topPos + gap * 1, FONT_COLOR, false);
-        guiGraphics.drawString(font, "Crew:", leftPos, topPos + gap * 2, FONT_COLOR, false);
-        guiGraphics.drawString(font, "Speed " + unit + ":", leftPos, topPos + gap * 3, FONT_COLOR, false);
-        guiGraphics.drawString(font, "Damage:", leftPos, topPos + gap * 4, FONT_COLOR, false);
-        guiGraphics.drawString(font, attachment, leftPos, topPos + gap * 5, FONT_COLOR, false);
-
-        guiGraphics.drawString(font, name, leftPos2, topPos + gap * 0, FONT_COLOR, false);
-        guiGraphics.drawString(font, smallShipType, leftPos2, topPos + gap * 1, FONT_COLOR, false);
-        guiGraphics.drawString(font, currentPassengers + "/" + maxPassengers, leftPos2, topPos + gap * 2, FONT_COLOR, false);
-        guiGraphics.drawString(font, currentSpeed + "/" + maxSpeed, leftPos2, topPos + gap * 3, FONT_COLOR, false);
-        guiGraphics.drawString(font, dmg + "%", leftPos2, topPos + gap * 4, FONT_COLOR, false);
-        guiGraphics.drawString(font, currentAttachment + "/" + maxAttachment, leftPos2, topPos + gap * 5, FONT_COLOR, false);
+        guiGraphics.drawString(font, currentPassengers + "/" + maxPassengers, leftPos2, topPos + gap * 0, FONT_COLOR, false);
+        guiGraphics.drawString(font, currentSpeed + "/" + maxSpeed, leftPos2, topPos + gap * 1, FONT_COLOR, false);
+        guiGraphics.drawString(font, dmg + "%", leftPos2, topPos + gap * 2, FONT_COLOR, false);
 
         guiGraphics.pose().popPose();
 
