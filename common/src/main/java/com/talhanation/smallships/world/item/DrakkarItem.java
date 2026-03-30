d6c76261a6427b95e60febff40f6224a3b872be8
Watherfoxx <46624490+Watherfoxx@users.noreply.github.com>
2026-03-30 15:24:07 +0200

    Fix ShipItem final getMaxDamage override error

--- common/src/main/java/com/talhanation/smallships/world/item/DrakkarItem.java
+++ common/src/main/java/com/talhanation/smallships/world/item/DrakkarItem.java
@@ -1,6 +1,5 @@
 package com.talhanation.smallships.world.item;
 
-import com.talhanation.smallships.config.SmallShipsConfig;
 import com.talhanation.smallships.world.entity.ship.DrakkarEntity;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.level.Level;
@@ -15,10 +14,5 @@ public class DrakkarItem extends ShipItem {
     @Override
     protected @NotNull Boat getBoat(@NotNull Level level, @NotNull HitResult hitResult) {
         return DrakkarEntity.summon(level, hitResult.getLocation().x, hitResult.getLocation().y, hitResult.getLocation().z);
-    }
-
-    @Override
-    protected int getShipMaxHealth() {
-        return SmallShipsConfig.Common.shipAttributeDrakkarMaxHealth.get().intValue();
     }
 }
